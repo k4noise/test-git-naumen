c314d243bd1808a275ecdff026528d84f052e5d1
Almazova Vlada <almazova.2004@mail.ru>
2024-09-22 22:47:56 +0500

    добавление поля название фичи

--- src/main/java/org/example/Ficha.java
+++ src/main/java/org/example/Ficha.java
@@ -2,7 +2,11 @@ package org.example;
 
 public class Ficha {
     public int countFich;
+
+    public String nameFicha;
+
     public Ficha(){
         countFich = 25;
+        nameFicha = "Task2";
     }
 }
